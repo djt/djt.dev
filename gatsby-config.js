fe84e8c88cc39b185e8a74bb972727562ae950ca
Daniel Treadwell <daniel@djt.id.au>
2021-01-08 14:54:31 +1000

    Add analytics.

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,11 @@ module.exports = {
         name: `twitter`,
         url: `https://twitter.com/djtdotdev`,
       },
+      {
+        name: `mailto`,
+        url: `mailto:daniel@djt.dev`,
+      },
+
     ],
   },
   plugins: [
@@ -46,5 +51,34 @@ module.exports = {
       options: {
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "G-RT2W08CTB8",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        optimizeId: "G-RT2W08CTB8",
+        // Enables Google Optimize Experiment ID
+        experimentId: "G-RT2W08CTB8",
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: "0",
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "djt.dev",
+      }
+    },    
   ],
 };
